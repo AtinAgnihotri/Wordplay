7f0c664c14a370ce767c5f611120240bdfc0347e
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-06 13:42:25 +0530

    Initial Commit

---
+++ Wordplay/WordplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordplayApp.swift
+//  Wordplay
+//
+//  Created by Atin Agnihotri on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WordplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
