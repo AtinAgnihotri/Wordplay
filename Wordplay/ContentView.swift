a5d548f303bcc427191c4e8a71b33dfe28b9ecfc
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-06 22:00:41 +0530

    Prototyping with UITextChecker

--- Wordplay/ContentView.swift
+++ Wordplay/ContentView.swift
@@ -12,17 +12,38 @@ struct ContentView: View {
     let people  = ["Tom", "Dick", "Hardy"]
     
     var body: some View {
+        var stuffWasLoaded = false
         var message = ""
+        var wordList: [String] = []
+        
+        // Text Checker
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        
         if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             message = "We found the file"
             if let fileContent = try? String(contentsOf: fileURL) {
                 message += "\nWe've loaded the file content"
+                stuffWasLoaded = true
+                wordList = fileContent.components(separatedBy: "\n")
+                var fistLetter = wordList.randomElement()!
+                let trimmed = fistLetter.trimmingCharacters(in: .whitespacesAndNewlines)
+            } else {
+                wordList = [message, "We couldn't load the file content"]
+//                message += "\nWe couldn't load the file content"
             }
         } else {
-            message = "File was not found"
+            wordList = ["File Not found"]
         }
         
-        return Text(message)
+        return List(wordList, id: \.self) {
+            Text("\($0)")
+        }
+        
+//        return stuffWasLoaded ? List{ ForEach(wordList, id: \.self){ Text("\($0)")}} : List { Text(message)}
     }
 }
 
