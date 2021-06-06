773b940a9afc26e9c31cfe4126be1d0e2fb7fde8
Atin Agnihotri <atin.agnihotri@paytmmoney.com>
2021-06-06 22:22:00 +0530

    Add UIApplicationExtensions

--- Wordplay/ContentView.swift
+++ Wordplay/ContentView.swift
@@ -9,41 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var userWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     let people  = ["Tom", "Dick", "Hardy"]
     
     var body: some View {
-        var stuffWasLoaded = false
-        var message = ""
-        var wordList: [String] = []
-        
-        // Text Checker
-        let word = "swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
-        
-        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-            message = "We found the file"
-            if let fileContent = try? String(contentsOf: fileURL) {
-                message += "\nWe've loaded the file content"
-                stuffWasLoaded = true
-                wordList = fileContent.components(separatedBy: "\n")
-                var fistLetter = wordList.randomElement()!
-                let trimmed = fistLetter.trimmingCharacters(in: .whitespacesAndNewlines)
-            } else {
-                wordList = [message, "We couldn't load the file content"]
-//                message += "\nWe couldn't load the file content"
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                List (userWords, id: \.self) {
+                    Text($0)
+                        .padding()
+                }
             }
-        } else {
-            wordList = ["File Not found"]
+            .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        var userAns = newWord
+                            .lowercased()
+                            .trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard userAns.count > 0 else {
+            return
         }
         
-        return List(wordList, id: \.self) {
-            Text("\($0)")
-        }
+        // other validations
+        userWords.insert(userAns, at: 0)
+        newWord = ""
         
-//        return stuffWasLoaded ? List{ ForEach(wordList, id: \.self){ Text("\($0)")}} : List { Text(message)}
     }
 }
 
--- Wordplay/WordplayApp.swift
+++ Wordplay/WordplayApp.swift
@@ -12,6 +12,7 @@ struct WordplayApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
         }
     }
 }
